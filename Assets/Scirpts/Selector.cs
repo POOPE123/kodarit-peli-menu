b1f7076d9c388f1d6990974e8cef8fcea3360055
POOPE123 <vaino.o.hanninen@gmail.com>
2026-03-31 18:17:23 +0300

    add scene change

--- Assets/Scirpts/Selector.cs
+++ Assets/Scirpts/Selector.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
 
 public class Selector : MonoBehaviour
 {
@@ -8,17 +11,73 @@ public class Selector : MonoBehaviour
     public Color selectColor = Color.yellow;
 
     public TMP_Text[] items; // Meidän napit listassa
+    public Color[] colors;
     private int index = 0;
+
+    public FadeControl fade;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        ChangeColors();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            index = (index - 1 + items.Length) % items.Length;
+            ChangeColors();
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            index = (index + 1) % items.Length;
+            ChangeColors();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space))
+        {
+            ActivateItem(index);
+        }
+    }
+
+    public void ActivateItem(int i)
+    {
+        Debug.Log("Painoit nappia: " + items[i].text);
+
+        if (items[i].text == "PLAY")
+        {
+            StartCoroutine(ChangeScenes("Minigames"));
+        }
+        else if (items[i].text == "Settings")
+        {
+            StartCoroutine(ChangeScenes("Settings"));
+        }
+    }
+
+    private IEnumerator ChangeScenes(string sceneName)
+    {
+        fade.FadeIn();
+
+        yield return new WaitForSeconds(fade.fadeDuration);
+
+        SceneManager.LoadScene(sceneName);
+    }
+
+    void ChangeColors()
+    {
+        for (int i = 0; i < items.Length; i++)
+        {
+            if(i == index)
+            {
+                items[i].color = selectColor;
+            }
+            else
+            {
+                items[i].color = colors[i];
+            }
+        }
     }
 }
